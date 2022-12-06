4a0771bb6916e158b5b2583346b222b1c34e719f
steff <steffyoroian@gmail.com>
2022-12-07 00:54:00 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Oroian_Stefania_Lab7;
+﻿using System;
+using Oroian_Stefania_Lab7.Data;
+using System.IO;
+
+
+namespace Oroian_Stefania_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
